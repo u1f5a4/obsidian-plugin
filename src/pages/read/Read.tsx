b849ec5a2825a5247dfbb72000b930ac070c0ec9
u1f5a4 <73600007+u1f5a4@users.noreply.github.com>
2024-06-20 15:18:29 +0300

    feat: implement read/create/edit calendar event

---
+++ src/pages/read/Read.tsx
@@ -0,0 +1,75 @@
+import { formatDuration, intervalToDuration } from "date-fns";
+import { useRxData } from "rxdb-hooks";
+
+import { closeModal, openModal } from "@/app/lib/plugin";
+import { CalendarEvent, deleteEvent } from "@/entities/event";
+
+import "./style.scss";
+
+export interface ReadProps {
+  eventClickId: string;
+}
+
+export const Read = ({ eventClickId }: ReadProps) => {
+  const { result, isFetching } = useRxData<CalendarEvent>(
+    "events",
+    collection => collection.findOne({ selector: { id: eventClickId } }),
+  );
+  if (isFetching) return <div>Loading...</div>;
+
+  const event = result[0];
+  if (!event) return <div>No event</div>;
+
+  const getDuration = () => {
+    const duration = intervalToDuration({
+      start: new Date(event.start),
+      end: new Date(event.end),
+    });
+
+    return formatDuration(duration);
+  };
+
+  const handleDeleteEvent = async () => {
+    await deleteEvent(event.id);
+    closeModal();
+  };
+
+  const handleEditEvent = async () => {
+    openModal("editEvent", { eventId: event.id });
+  };
+
+  return (
+    <div className="read">
+      <h1>Read</h1>
+
+      <label>
+        Title:
+      </label>
+      <p>{event.title}</p>
+
+      <label>
+        Duration:
+      </label>
+      <p>{getDuration()}</p>
+
+      <div>
+        <label>
+          Start:
+        </label>
+        <p>{event.start}</p>
+      </div>
+
+      <div>
+        <label>
+          End:
+        </label>
+        <p>{event.end}</p>
+      </div>
+
+      <div>
+        <button onClick={handleDeleteEvent}>Delete</button>
+        <button onClick={handleEditEvent}>Edit</button>
+      </div>
+    </div>
+  );
+};
---
+++ src/shared/ui/CSelectTime.tsx
@@ -0,0 +1,23 @@
+import { Control, Controller, FieldError, FieldValues, Path, RegisterOptions } from "react-hook-form";
+
+type CSelectTimeProps<T extends FieldValues> = {
+  name: Path<T>;
+  control: Control<T>;
+  rules?: RegisterOptions<T>;
+  errors: FieldError | undefined;
+};
+
+export const CSelectTime = <T extends FieldValues>({ name, control, rules, errors }: CSelectTimeProps<T>) => {
+  const style = {
+    border: errors ? "2px solid red" : undefined,
+  };
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      rules={rules}
+      render={({ field }) => <input style={style} type="time" {...field} />}
+    />
+  );
+};
