1709d181b5140e1c183e132810177c5a09fcf555
u1f5a4 <73600007+u1f5a4@users.noreply.github.com>
2024-06-22 07:42:24 +0300

    fix: remove delay on load plugin

--- src/app/lib/plugin/plugin.ts
+++ src/app/lib/plugin/plugin.ts
@@ -5,22 +5,13 @@ import { initModal } from "./pluginModal"
 import { initSettingTab } from "./pluginSettingTab"
 import { initSidebarView } from "./pluginSidebarView"
 
-function delay(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
-
 export default class MyPlugin extends Plugin {
   async onload() {
-    await delay(300)
-    await initMainView(this)
-
-    await delay(100)
-    await initSidebarView(this)
-
-    await delay(100)
-    await initSettingTab(this)
-
-    await delay(100)
-    initModal(this)
+    this.app.workspace.onLayoutReady(async () => {
+      await initMainView(this)
+      await initSidebarView(this)
+      await initSettingTab(this)
+      await initModal(this)
+    })
   }
 }
